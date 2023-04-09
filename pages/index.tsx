8cc8db99c299595f0f340ede63a50d5791fbae70
rosuH <hi@rosuh.me>
2023-04-10 00:36:46 +0800

    :rocket:

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,21 @@ import styles from "@/styles/Home.module.css";
 import React, { useEffect } from "react";
 import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
+import ReactPlayer from "react-player";
+import marketImage from "../public/installation_button.svg";
+// import videoExplainCode from '../public/assets/explain_code_eng.mp4'
+// import videoCodeGen from '../public/installation_button.svg'
+// import videoCodeOptimization from '../public/installation_button.svg'
+// import videoDocGen from '../public/installation_button.svg'
 
 export default function Home() {
   return (
     <>
       <Script
         async
-        src="https://plugins.jetbrains.com/assets/scripts/mp-widget.js"
-        onLoad={() => {
-          // @ts-ignore
-          MarketplaceWidget.setupMarketplaceWidget(
-            "install",
-            21289,
-            "#jetbrainMarket"
-          );
-        }}
-      />
-      <Script 
-        async 
         src="https://www.googletagmanager.com/gtag/js?id=G-HEJVZLQ4GV"
       />
-      <Script id="google-analytics" strategy="afterInteractive"> 
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
@@ -38,25 +32,25 @@ export default function Home() {
         <title>AICommit</title>
         <meta
           name="description"
-          content="AICommit automates commit messages and streamlines the development process for developers."
+          content="AI-powered programming assistant for JetBrains IDEs!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type=" image/svg+xml" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="theme-color" content="#4a4a4a" />
         <meta property="og:title" content="Automates your commit by AI!" />
-        <meta name="google-site-verification" content="XYctT5gtc4q0PyFyA7mLFRlGQxCplYC5XM_SBLjdV6Y" />
+        <meta
+          name="google-site-verification"
+          content="XYctT5gtc4q0PyFyA7mLFRlGQxCplYC5XM_SBLjdV6Y"
+        />
         <meta
           property="og:description"
-          content="AICommit automates commit messages and streamlines the development process for developers."
+          content="AI-powered programming assistant for JetBrains IDEs!"
         />
-        <meta
-          property="og:image"
-          content="/og-image.png"
-        />
+        <meta property="og:image" content="/og-image.jpg" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
+        <div className={styles.aicommitLogo}>
           <a
             href="https://aicommit.app"
             target="_blank"
@@ -71,24 +65,94 @@ export default function Home() {
               priority
             />
           </a>
-          <div>
-            <h1>AICommit</h1>
-            <h2>Intellij Platform Plugin</h2>
-          </div>
         </div>
 
-        <div className={styles.video}>
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/sDG8cx6i_kM"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
+        <div className={styles.description}>
+          <div>
+            <h1 className={styles.headline}>AICommit</h1>
+            <h2>
+              <a href="https://plugins.jetbrains.com/plugin/21289-aicommit">
+                AI-powered programming assistant for JetBrains IDEs!
+              </a>
+            </h2>
+          </div>
         </div>
-        <div className={styles.jetbrainMarket} id="jetbrainMarket"></div>
+        <div className={styles.jetbrainMarket} id="jetbrainMarket">
+          <a
+            href="https://plugins.jetbrains.com/plugin/8579-translation"
+            target="_blank"
+          >
+            <Image
+              src={marketImage}
+              alt="Get from Marketplace"
+              width={200}
+              height={50}
+              title="Get from Marketplace"
+            />
+          </a>
+        </div>
+        <div className={styles.mainDescribtion}>
+          <p>
+            <a href="https://plugins.jetbrains.com/plugin/21289-aicommit">
+              AICommit
+            </a>{" "}
+            is a JetBrains IDE plugin based on OpenAI GPT, which provides a
+            range of intelligent coding features, including automated commit
+            message generation, code optimization, code interpretation,
+            documentation generation, code conversion, and translation. Make
+            your coding process more efficient and convenient.
+          </p>
+        </div>
+        <div className={styles.features}>
+          <h2>What can AICommit Plugin do?</h2>
+          <div>
+            <h3>Generate Commit Message</h3>
+            <video loop autoPlay muted controls>
+              <source src="/assets/commit_vcs_window.mp4" />
+            </video>
+          </div>
+          <div>
+            <h3>Explain Code</h3>
+            <video loop autoPlay muted controls>
+              <source src="/assets/explain_code_eng.mp4" />
+            </video>
+          </div>
+          <div>
+            <h3>Code Translate</h3>
+            <video loop autoPlay muted controls>
+              <source src="/assets/code_translate_eng.mp4" />
+            </video>
+          </div>
+          <div>
+            <h3>Code Optimization</h3>
+            <video loop autoPlay muted controls>
+              <source src="/assets/code_optimization_eng.mp4" />
+            </video>
+          </div>
+          <div>
+            <h3>Doc Generation</h3>
+            <video loop autoPlay muted controls>
+              <source src="/assets/doc_gen_eng.mp4" />
+            </video>
+          </div>
+          <div>
+            <h3>Started Guide</h3>
+            <div className={styles.video}>
+              <iframe
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/sDG8cx6i_kM"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        </div>
+        <div className={styles.bottomHeader}>
+          <h2>More than that...</h2>
+        </div>
         <div className={styles.grid}>
           <a
             href="https://plugins.jetbrains.com/plugin/21289-aicommit"
